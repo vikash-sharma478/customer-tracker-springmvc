42f3fe3261e3c63f15217682b2e22ea3dd34f789
Vikash-Sharma <157942515+vikash-sharma478@users.noreply.github.com>
2025-06-29 16:11:14 +0530

    Create 02-customer-tracker.sql

---
+++ SQL script/02-customer-tracker.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`;
+
+USE `web_customer_tracker`;
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `customer` WRITE;
+
+INSERT INTO `customer` VALUES 
+	(1,'David','Adams','david@gmail.com'),
+	(2,'John','Doe','john@outlook.com'),
+	(3,'Ajay','Rao','ajay@gmail.com'),
+	(4,'Mary','Public','mary@facebook.com'),
+	(5,'Maxwell','Dixon','max@hotmail.com');
