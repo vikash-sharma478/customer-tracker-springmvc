3bf17388bfefa30a9b51a02d0a5435b42c7dab20
Vikash-Sharma <157942515+vikash-sharma478@users.noreply.github.com>
2025-06-29 16:08:18 +0530

    Update 01-creat-user.sql

--- SQL script/01-creat-user.sql
+++ SQL script/01-creat-user.sql
@@ -1,1 +1,9 @@
+-- Create a MySQL user for your CRM application
+CREATE USER 'vikashcrm'@'localhost' IDENTIFIED BY 'vikashcrm123';
+
+-- Grant full privileges to that user on all databases (you can limit it later)
+GRANT ALL PRIVILEGES ON *.* TO 'vikashcrm'@'localhost';
+
+-- Ensure compatibility with MySQL native password authentication
+ALTER USER 'vikashcrm'@'localhost' IDENTIFIED WITH mysql_native_password BY 'vikashcrm123';
 
